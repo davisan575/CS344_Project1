e4353fd4a16d71cc2a59454a757e5c02ae095ccc
Alison <alison@Alisons-MacBook-Pro-1.local>
2019-03-27 17:00:50 -0400

    combined into one header file

--- Integer.cpp
+++ Integer.cpp
@@ -1,24 +1,3 @@
 #include <iostream>
 #include "Integer.h"
 
-
-extern int Integer_count;
-bool operator<(const Integer & compare_left, const Integer & compare_right)
-{
-    Integer_count++;
-    if(compare_left.value() < compare_right.value())
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-Integer & Integer::operator=(const Integer & prev_Integer)
-{
-    Integer_count++;
-    value_ = prev_Integer.value();
-    return *this;
-}
--- Integer.h
+++ Integer.h
@@ -28,5 +28,23 @@ inline Integer::Integer(const Integer & i1)
 }
 
 
+bool operator<(const Integer & compare_left, const Integer & compare_right)
+{
+    Integer_count++;
+    if(compare_left.value() < compare_right.value())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
+Integer & Integer::operator=(const Integer & prev_Integer)
+{
+    Integer_count++;
+    value_ = prev_Integer.value();
+    return *this;
+}
 #endif
